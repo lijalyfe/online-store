0973ae38819d11523c473d8ca76a5ddc31bd95d5
lija_lyfe <lija6556@list.ru>
2023-12-04 22:28:50 +0500

    change models.py

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -5,15 +5,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Дополнительные поля и методы могут быть добавлены по вашему усмотрению
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    # Дополнительные поля и методы могут быть добавлены по вашему усмотрению
-
