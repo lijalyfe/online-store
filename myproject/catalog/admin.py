0208ea01a9a8dad003aef8c093b5acae02c6253b
lija_lyfe <lija6556@list.ru>
2023-12-04 22:28:15 +0500

    setting up the display in the administrative panel

--- myproject/catalog/admin.py
+++ myproject/catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+admin.site.register(Product, ProductAdmin)
